64d82838c69e3f1893b517d5028fc8c08cbe16fa
KostasChas <kostaschas@gmail.com>
2021-02-19 20:15:32 +0200

    Add project files.

---
+++ NotePad/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace NotePad
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Titletxt = new System.Windows.Forms.TextBox();
+            this.textMessage = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnnew = new System.Windows.Forms.Button();
+            this.btnsave = new System.Windows.Forms.Button();
+            this.btnread = new System.Windows.Forms.Button();
+            this.btndelete = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.DarkOrange;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label1.Font = new System.Drawing.Font("Perpetua Titling MT", 14F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(142, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "TITLE";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.DarkOrange;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label2.Font = new System.Drawing.Font("Perpetua Titling MT", 14F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))));
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Location = new System.Drawing.Point(115, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(128, 31);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Message";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Titletxt
+            // 
+            this.Titletxt.AcceptsReturn = true;
+            this.Titletxt.Location = new System.Drawing.Point(68, 61);
+            this.Titletxt.Name = "Titletxt";
+            this.Titletxt.Size = new System.Drawing.Size(240, 22);
+            this.Titletxt.TabIndex = 2;
+            // 
+            // textMessage
+            // 
+            this.textMessage.Location = new System.Drawing.Point(68, 166);
+            this.textMessage.Multiline = true;
+            this.textMessage.Name = "textMessage";
+            this.textMessage.Size = new System.Drawing.Size(240, 186);
+            this.textMessage.TabIndex = 3;
+            this.textMessage.TextChanged += new System.EventHandler(this.textMessage_TextChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(438, 61);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(199, 291);
+            this.dataGridView1.TabIndex = 4;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // btnnew
+            // 
+            this.btnnew.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnnew.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnnew.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnnew.FlatAppearance.BorderSize = 4;
+            this.btnnew.ForeColor = System.Drawing.Color.White;
+            this.btnnew.Location = new System.Drawing.Point(68, 378);
+            this.btnnew.Name = "btnnew";
+            this.btnnew.Size = new System.Drawing.Size(90, 40);
+            this.btnnew.TabIndex = 6;
+            this.btnnew.Text = "New";
+            this.btnnew.UseVisualStyleBackColor = false;
+            this.btnnew.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnsave
+            // 
+            this.btnsave.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnsave.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnsave.ForeColor = System.Drawing.Color.White;
+            this.btnsave.Location = new System.Drawing.Point(218, 378);
+            this.btnsave.Name = "btnsave";
+            this.btnsave.Size = new System.Drawing.Size(90, 40);
+            this.btnsave.TabIndex = 6;
+            this.btnsave.Text = "Save";
+            this.btnsave.UseVisualStyleBackColor = false;
+            this.btnsave.Click += new System.EventHandler(this.btnsave_Click);
+            // 
+            // btnread
+            // 
+            this.btnread.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnread.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnread.ForeColor = System.Drawing.Color.White;
+            this.btnread.Location = new System.Drawing.Point(438, 378);
+            this.btnread.Name = "btnread";
+            this.btnread.Size = new System.Drawing.Size(90, 40);
+            this.btnread.TabIndex = 7;
+            this.btnread.Text = "Read";
+            this.btnread.UseVisualStyleBackColor = false;
+            this.btnread.Click += new System.EventHandler(this.btnread_Click);
+            // 
+            // btndelete
+            // 
+            this.btndelete.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btndelete.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btndelete.ForeColor = System.Drawing.Color.White;
+            this.btndelete.Location = new System.Drawing.Point(547, 378);
+            this.btndelete.Name = "btndelete";
+            this.btndelete.Size = new System.Drawing.Size(90, 40);
+            this.btndelete.TabIndex = 8;
+            this.btndelete.Text = "Delete";
+            this.btndelete.UseVisualStyleBackColor = false;
+            this.btndelete.Click += new System.EventHandler(this.btndelete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = global::NotePad.Properties.Resources.notpic;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(700, 513);
+            this.Controls.Add(this.btndelete);
+            this.Controls.Add(this.btnread);
+            this.Controls.Add(this.btnsave);
+            this.Controls.Add(this.btnnew);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textMessage);
+            this.Controls.Add(this.Titletxt);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(718, 560);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(718, 560);
+            this.Name = "Form1";
+            this.Text = "SimpleNotePad";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Titletxt;
+        private System.Windows.Forms.TextBox textMessage;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnnew;
+        private System.Windows.Forms.Button btnsave;
+        private System.Windows.Forms.Button btnread;
+        private System.Windows.Forms.Button btndelete;
+    }
+}
+
---
+++ NotePad/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NotePad
+{
+    public partial class Form1 : Form
+    {
+
+        DataTable table;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Titletxt.Clear();
+            textMessage.Clear();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            table = new DataTable();
+            table.Columns.Add("Title", typeof(string));
+            table.Columns.Add("Messages", typeof(string));
+            dataGridView1.DataSource = table;
+            dataGridView1.Columns["Messages"].Visible = false;
+            dataGridView1.Columns["Title"].Width = 196;
+
+
+        }
+
+        private void btnsave_Click(object sender, EventArgs e)
+        {
+            table.Rows.Add(Titletxt.Text, textMessage.Text);
+            Titletxt.Clear();
+            textMessage.Clear();
+        }
+
+        private void btnread_Click(object sender, EventArgs e)
+        {
+            int index = dataGridView1.CurrentCell.RowIndex;
+
+            if (index > -1)
+            {
+                Titletxt.Text = table.Rows[index].ItemArray[0].ToString();
+                textMessage.Text = table.Rows[index].ItemArray[1].ToString();
+            }
+
+
+
+        }
+
+        private void btndelete_Click(object sender, EventArgs e)
+        {
+            int index = dataGridView1.CurrentCell.RowIndex;
+
+            table.Rows[index].Delete();
+
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void textMessage_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
